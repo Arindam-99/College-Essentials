142b9847e77ce1a1b1da37a246e017be9b5f8d8d
arindam <dasarindam342@gmail.com>
2025-09-03 21:13:39 +0530

    Uploaded Assignment-9

---
+++ Fifth Semester/ML(Machine Learning)/Assignment-9/first.py
@@ -0,0 +1,16 @@
+#Library Function 
+import pandas as pd 
+import seaborn as sns
+import plotly.express as px
+
+#Load Titanic dataset from seaborn 
+df = sns.load_dataset('titanic')
+
+# Interactive bar chart: Survival count by gender 
+fig = px.bar(df,x='sex',color ="survived",barmode='group',title="Survival by Gender")
+fig.show()
+
+
+#Interactive scatter plot: Age vs Fare 
+fig2 = px.scatter(df,x='Age',y='fare', color='pclass',size="fare", hover_name='sex',title= 'Age vs Fare with class ')
+fig2.show()
